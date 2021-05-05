e88d84c3f1710a2fc4240b15db06aa653a5d8061
JaechanGo <happypuki@gmail.com>
2021-05-05 19:06:42 +0900

    Product_dto

---
+++ src/dto/Product.java
@@ -0,0 +1,98 @@
+package dto;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = -4274700572038677000L;
+
+	private String productId;
+	private String pname;	
+	private Integer unitPrice; 
+	private String notice; 
+	private String coffee;
+	private String description; 	
+	private Integer gram;
+	private String image;
+	private Integer orderQnt; //주문수량 
+	
+	public Product() {
+		super();
+	}
+
+	public Product(String productId, String pname, Integer unitPrice) {
+		this.productId = productId;
+		this.pname = pname;
+		this.unitPrice = unitPrice;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getNotice() {
+		return notice;
+	}
+
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+
+	public String getCoffee() {
+		return coffee;
+	}
+
+	public void setCoffee(String coffee) {
+		this.coffee = coffee;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	public Integer getGram() {
+		return gram;
+	}
+
+	public void setGram(Integer gram) {
+		this.gram = gram;
+	}
+	public void setOrderQnt(int orderQnt) {
+		this.orderQnt = orderQnt;
+	}
+	public int getOrderQnt() {
+		return orderQnt;
+	}
+}
