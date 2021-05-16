102c32fcfde449d6505380ed3f2cf0babd85a527
JaechanGo <happypuki@gmail.com>
2021-05-16 23:22:25 +0900

    payment_DTO_java

---
+++ src/payment/payment.java
@@ -0,0 +1,92 @@
+package payment;
+
+public class payment {
+	private Integer id;
+	private String userID;
+	private String ProductID;
+	private Integer ProductQnt;
+	private String userEmail;
+	private String userPhone;
+	private String itemuser_ID;
+	private String itemAdress;
+	private String itemPhone;
+	private String itemMessage;
+	private String date;
+	private String state;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getProductID() {
+		return ProductID;
+	}
+	public void setProductID(String productID) {
+		ProductID = productID;
+	}
+	public Integer getProductQnt() {
+		return ProductQnt;
+	}
+	public void setProductQnt(Integer productQnt) {
+		ProductQnt = productQnt;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getItemuser_ID() {
+		return itemuser_ID;
+	}
+	public void setItemuser_ID(String itemuser_ID) {
+		this.itemuser_ID = itemuser_ID;
+	}
+	public String getItemAdress() {
+		return itemAdress;
+	}
+	public void setItemAdress(String itemAdress) {
+		this.itemAdress = itemAdress;
+	}
+	public String getItemPhone() {
+		return itemPhone;
+	}
+	public void setItemPhone(String itemPhone) {
+		this.itemPhone = itemPhone;
+	}
+	public String getItemMessage() {
+		return itemMessage;
+	}
+	public void setItemMessage(String itemMessage) {
+		this.itemMessage = itemMessage;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	
+
+}
